e4f5467661ef34fd9e6629d616896225bdf23012
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-10-31 23:08:48 -0500

    Added CartIcon and replaced plain button tag with external component and its CSS.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = props => {
   return (
     <>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <button>Card</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="Table full of delicious food" />
