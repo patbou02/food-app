21a2edb5d347be4b3f4a2eac66cc537a6a6ff299
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-11-02 14:51:39 -0500

    Added context to CartButton and removed hard coded cart # value.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
   //console.log('HeaderCartButton props', props);
+  const context = useContext(CartContext);
+  //console.log('context', context)
+
+  // context.items.length could b used depending on how the cart content
+  // is populated. In this case, if we have 4 Sushi's we want the cart to
+  // show 1 Object (sushi) of which we have 4 items within the Obj as in {item.amount}.
+  // In this case, it would still show as 1 item hence length() would be incorrect.
+  // Because of that, using a reducer will be more appropriate.
+  let numberOfItems = context.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 };
