cdcb8ecd606f3c9599773c36e3f6ec1f542b0103
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-11-27 03:05:50 -0800

    Added checkout form and display it when clicking on order button otherwise display the modal actions.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = props => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const context = useContext(CartContext);
 
   const totalAmount = `$${context.totalAmount.toFixed(2)}`;
@@ -12,6 +14,8 @@ const Cart = props => {
 
   const cartItemAddHandler = item => context.addItem({...item, amount: 1});
   const cartItemRemoveHandler = id => context.removeItem(id);
+
+  const orderHandler = () => setIsCheckout(true);
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -26,6 +30,18 @@ const Cart = props => {
     </ul>
   );
 
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+      {hasItems && (
+        <button
+          className={classes.button}
+          onClick={orderHandler}
+        >Order</button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -33,10 +49,7 @@ const Cart = props => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-        {hasItems && (<button className={classes.button}>Order</button>)}
-      </div>
+      {isCheckout ? <Checkout onCancel={props.onClose} /> : modalActions}
     </Modal>
   );
 };
